7a659aeb8ecc13ddf3ead59fdc351b5ed8d7acee
Vladimir Samoilenko <vladimir.samoilenko@cgm.com>
2023-05-17 18:23:42 +0200

    Inital commit

---
+++ NukeX/NukeX.cs
@@ -0,0 +1,75 @@
+﻿using System.Security.Cryptography;
+
+namespace NukeX;
+
+public static class NukeXHelper
+{
+    // find from current directory and upper folder which contains special folder name 
+    public static string? FindRootDirectory(string specialFolderName)
+    {
+        var currentDirectory = Directory.GetCurrentDirectory();
+        var rootDirectory = currentDirectory;
+        while (true)
+        {
+            if (Directory.Exists(Path.Combine(rootDirectory, specialFolderName)))
+            {
+                return rootDirectory;
+            }
+
+            var parentDirectory = Directory.GetParent(rootDirectory);
+            if (parentDirectory == null)
+            {
+                return null;
+            }
+
+            rootDirectory = parentDirectory.FullName;
+        }
+    }
+
+    // function to find files by mask in specified folder
+    public static IEnumerable<string> FindFiles(string folder, string mask)
+    {
+        var files = Directory.GetFiles(folder, mask, SearchOption.TopDirectoryOnly);
+        return files;
+    }
+
+
+    public static IEnumerable<string> GetFiles(string path, string[] excludes)
+    {
+        path = Path.GetFullPath(path);
+        foreach (var file in new DirectoryInfo(path).GetFiles("*.*", SearchOption.AllDirectories))
+        {
+            var isExclude = false;
+            foreach (var exclude in excludes)
+            {
+                if (file.FullName.StartsWith(Path.Combine(path, exclude), StringComparison.InvariantCultureIgnoreCase))
+                {
+                    isExclude = true;
+                    continue;
+                }
+            }
+
+            if (isExclude)
+            {
+                continue;
+            }
+            yield return file.FullName;
+        }
+    }
+    
+    
+    // function to recieve list of file and calculate hash for all tougether
+    public static string CalculateHash(IEnumerable<string> files)
+    {
+        var hash = SHA256.Create();
+        foreach (var file in files)
+        {
+            var bytes = File.ReadAllBytes(file);
+            hash.TransformBlock(bytes, 0, bytes.Length, null, 0);
+        }
+
+        hash.TransformFinalBlock(new byte[0], 0, 0);
+        return BitConverter.ToString(hash.Hash).Replace("-", "").ToLowerInvariant();
+    }
+    
+}
---
+++ NukeX/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+using NukeX;
+
+var sw = Stopwatch.StartNew();
+
+Console.WriteLine("*** NukeX ***");
+
+var rootFolder = NukeXHelper.FindRootDirectory(".nuke");
+if (rootFolder != null)
+{
+    Console.WriteLine("Nuke root folder: " + rootFolder);
+}
+else
+{
+    Console.WriteLine("Cannot find root .nuke folder");
+    Environment.Exit(1);
+    return;
+}
+
+var ps1FilePath = Path.Combine(rootFolder, "build.ps1");
+if (File.Exists(ps1FilePath))
+{
+    Console.WriteLine("Nuke build.ps1 file: " + ps1FilePath);
+}
+else
+{
+    Console.WriteLine("Cannot find build.ps1 file");
+    Environment.Exit(1);
+    return;
+}
+
+// read using regex from file ps1FilePath and find path. Line in file like this: $BuildProjectFile = "$PSScriptRoot\build\_build.csproj"
+var regex = new Regex(@"\$BuildProjectFile = ""\$PSScriptRoot\\(?<path>.*)""");
+var match = regex.Match(File.ReadAllText(ps1FilePath));
+string buildProjectFilePath = "";
+
+if (match.Success)
+{
+    buildProjectFilePath = Path.Combine(rootFolder, match.Groups["path"].Value);
+    Console.WriteLine("Nuke build project file: " + buildProjectFilePath);
+}
+else
+{
+    Console.WriteLine("Cannot find build project file");
+    Environment.Exit(1);
+    return;
+}
+
+var buildProjectDirectory = Path.GetDirectoryName(buildProjectFilePath);
+var buildProjectDirectoryOutput = Path.Combine(buildProjectDirectory, "bin", "NukeX");
+
+var fileList = NukeXHelper.GetFiles(buildProjectDirectory,
+    new[] { "bin" + Path.DirectorySeparatorChar, "obj" + Path.DirectorySeparatorChar }).ToList();
+var hash = NukeXHelper.CalculateHash(fileList);
+var buildProjectHashFilePath = Path.Combine(buildProjectDirectoryOutput, "build.hash");
+if (!File.Exists(buildProjectHashFilePath) || File.ReadAllText(buildProjectHashFilePath) != hash)
+{
+    if (File.Exists(buildProjectHashFilePath))
+    {
+        File.Delete(buildProjectHashFilePath);
+    }
+
+
+    Process.Start(new ProcessStartInfo("dotnet",
+            $"build \"{buildProjectFilePath}\" --configuration Release --output \"{buildProjectDirectoryOutput}\"")
+        {
+            UseShellExecute = false,
+        })
+        ?.WaitForExit();
+
+    File.WriteAllText(buildProjectHashFilePath, hash);
+}
+
+var buildProjectExeFile = NukeXHelper.FindFiles(buildProjectDirectoryOutput, "*.exe").FirstOrDefault();
+if (buildProjectExeFile != null)
+{
+    Console.WriteLine("Nuke build project exe file: " + buildProjectExeFile);
+}
+else
+{
+    Console.WriteLine("Cannot find build project exe file");
+    Environment.Exit(1);
+    return;
+}
+
+
+// execute buildProjectExeFile executable file with complete same arguments as this program (dot not forget escaping)
+var arguments = string.Join(" ", args.Select(x => $"\"{x}\""));
+
+
+sw.Stop();
+Console.WriteLine($"NukeX build time: {sw.ElapsedMilliseconds} ms");
+Process.Start(new ProcessStartInfo(buildProjectExeFile, arguments)
+    {
+        UseShellExecute = false,
+    })
+    ?.WaitForExit();
